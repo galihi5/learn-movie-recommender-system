373b906cfb7cebb50d46bbacab651c4cfa63defa
galihi5 <www.galih@gmail.com>
2022-08-02 20:52:29 +0700

    initial commit

---
+++ movie-recommender-system/src/test/java/com/gaw/movierecommendersystem/MovieRecommenderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaw.movierecommendersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
